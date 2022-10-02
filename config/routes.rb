0913b62b3190ef12abeb2b8ec373268bda49267f
Andrew Clarkson <94328514+andrew-clarkson@users.noreply.github.com>
2022-10-02 00:25:25 +0000

    added edit and destroy and links

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'blog#home'
   get 'about', to: 'blog#about'
-  resources :articles, only: [:show, :index]
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # above is same as below
+  resources :articles
 end
