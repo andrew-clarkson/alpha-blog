5f5109ce97ce41801dfdaca3a7610e8af15d8ff2
Andrew Clarkson <94328514+andrew-clarkson@users.noreply.github.com>
2022-10-12 00:13:37 +0000

    added user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # above is same as below, exposes all routes
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
